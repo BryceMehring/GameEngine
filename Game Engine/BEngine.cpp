1b97ae4e99074349812c166a0a6a56d31a621a2b
ColeXemi <ColeXemi@d47a555f-5172-324c-b6bc-1cff69a52d8b>
2011-03-03 07:04:41 +0000

    Started to Move more into the DX9 dll file. Expirmenting with global include dictionarys

--- Game Engine/BEngine.cpp
+++ Game Engine/BEngine.cpp
@@ -2,29 +2,28 @@
 
 #include "BEngine.h"
 #include "VertexStream.h"
-#include "BFont.h"
 
-BEngine* g_pEngine = NULL;
+IMPL_SINGLETON(BEngine);
 
 LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	BEngine* pEngine = BEngine::GetInstance();
+
 	// Don't start processing messages until the application has been created.
-	if(g_pEngine)
+	if(pEngine)
 	{
-		g_pEngine->MsgProc(msg, wParam, lParam);
+		pEngine->MsgProc(msg, wParam, lParam);
 	}
 
 	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
-BEngine* BEngine::s_pInstance;
-
 BEngine::BEngine(HINSTANCE hInstance,const string& winCaption)
 {
 	// Default values
-	m_hInstance = hInstance;
-	m_bPaused = false;
-	m_ClearBuffers = D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER;
+	m_hInstance    = hInstance;
+	m_bPaused      = false;
+	
 
 	// initialize timer 
 	__int64 cntsPerSec = 0;
@@ -35,7 +34,6 @@ BEngine::BEngine(HINSTANCE hInstance,const string& winCaption)
 	try
 	{
 		InitializeWindows(m_hInstance,winCaption);
-		InitializeDirectX();
 	}
 	catch(string error)
 	{
@@ -43,10 +41,7 @@ BEngine::BEngine(HINSTANCE hInstance,const string& winCaption)
 		exit(0);
 	}
 
-	// post-Initialize
-	D3DXFONT_DESC desc;
-	ZeroMemory(&desc,sizeof(desc));
-	D3DXCreateFontIndirect(m_p3Device,&desc,&m_pFont);
+	
 
 	// streams
 	/*m_p3Device->AddRef();
@@ -64,22 +59,40 @@ BEngine::BEngine(HINSTANCE hInstance,const string& winCaption)
 
 BEngine::~BEngine()
 {
-	m_pFont->Release();
-	m_pDirect3D->Release();
-	m_p3Device->Release();
-	//m_pEffectPool->Release();
-
-	//DeleteVertexStreams();
-
-	/*BCamera::Delete();
-	BFont::Delete();*/
+	
 }
 
-IDirect3DDevice9* BEngine::GetDevice()
+BEngine* BEngine::GetInstance()
 {
-	m_p3Device->AddRef();
-	return m_p3Device;
+	BEngine* pInstance = NULL;
+
+	if(s_pInstance)
+	{
+		pInstance = s_pInstance;
+	}
+
+	return pInstance;
 }
+
+void BEngine::Initialize(HINSTANCE hInstance,const string& winCaption)
+{
+	if(s_pInstance == NULL)
+	{
+		s_pInstance = new BEngine(hInstance,winCaption);
+	}
+}
+
+HINSTANCE BEngine::GetHINSTANCE() const
+{
+	return m_hInstance;
+}
+
+HWND BEngine::GetWindowHandle() const 
+{ 
+	return m_hWindowHandle;
+}
+
+
 
 bool BEngine::Update()
 {
@@ -94,12 +107,12 @@ bool BEngine::Update()
 	// If there are Window messages then process them.
 	if(PeekMessage( &msg, 0, 0, 0, PM_REMOVE ))
 	{
-        TranslateMessage( &msg );
-        DispatchMessage( &msg );
+		TranslateMessage( &msg );
+		DispatchMessage( &msg );
 	}
 	// Otherwise, do animation/game stuff.
 	else
-    {
+	{
 		// If the application is paused then free some CPU cycles to other 
 		// applications and then continue on to the next frame.
 		if( m_bPaused )
@@ -112,240 +125,12 @@ bool BEngine::Update()
 			// clear the screen, get ready to render
 			Begin();
 		}
-    }
-
-	return true;
-}
-
-void BEngine::RenderOptions()
-{
-	// Draw font, need to add this to a function
-	/*BFont& font = BFont::Instance();
-	ID3DXFont* pFont = font.GetFont("default");
-
-	RECT rec = {0,0,800,800};
-
-	ostringstream mode;
-
-	for(int i = 0; i < m_mode.size(); ++i)
-	{
-		mode << m_mode[i].Width << " x " << m_mode[i].Height << endl;
-	}
-
-	pFont->DrawText(0,mode.str().c_str(),-1,&rec,DT_WORDBREAK,D3DCOLOR_XRGB(255,255,255));*/
-}
-
-bool BEngine::Begin()
-{
-	if( !IsDeviceLost() )
-	{
-		m_p3Device->Clear(0,0,m_ClearBuffers,0,1.0f,0);
-		m_p3Device->BeginScene();
-
-		__int64 prevTimeStamp = 0;
-		QueryPerformanceCounter((LARGE_INTEGER*)&prevTimeStamp);
-
-		m_fStartCount = prevTimeStamp;
-
-		return true;
-	}
-	return false;
-}
-
-bool BEngine::End()
-{
-	__int64 currTimeStamp = 0;
-	QueryPerformanceCounter((LARGE_INTEGER*)&currTimeStamp);
-	m_fDT = (currTimeStamp - m_fStartCount)*m_fSecsPerCount;
-
-	m_p3Device->EndScene();
-		
-	return true;
-}
-
-void BEngine::DrawText(char* p)
-{
-	RECT r = {0,0,40,40};
-	m_pFont->DrawText(0,p,-1,&r,DT_NOCLIP,0xffffffff);
-}
-
-void BEngine::Present()
-{
-	// Before we present the screen to the screen, we must stop rendering to the back buffer
-	End();
-
-	m_p3Device->Present(0,0,0,0);
-}
-
-// 
-void BEngine::EnumerateDisplayAdaptors()
-{
-	D3DDISPLAYMODE mode;
-	for(int i = 0; i < m_pDirect3D->GetAdapterCount(); ++i)
-	{
-		UINT n = m_pDirect3D->GetAdapterModeCount(i,D3DFMT_X8R8G8B8);
-
-		for(int j = 0; j < n; ++j)
-		{
-			m_pDirect3D->EnumAdapterModes(i,D3DFMT_X8R8G8B8,j,&mode);
-			m_mode.push_back(mode);
-		}
-	}
-}
-
-/*bool BEngine::LoadEffect(UINT iID,const char* pFile)
-{
-	// 1. Check to see if the effect is already loaded
-	if(m_Effects.find(iID) == m_Effects.end())
-	{
-		// 2. Load the effect
-		ID3DXEffect* pEffect = 0;
-		ID3DXBuffer* pErrors = 0;
-		D3DXCreateEffectFromFile(m_p3Device,pFile,0,0,D3DXSHADER_DEBUG,m_pEffectPool,&pEffect,&pErrors);
-
-		// 3. If there are errors, deal with them
-		if(pErrors)
-		{
-			char* pMsg = static_cast<char*>(pErrors->GetBufferPointer());
-			MessageBox(0,pMsg,0,0);
-			PostQuitMessage(0);
-
-			return false;
-		}
-
-		// 4. Insert effect into data structure
-		m_Effects.insert(make_pair(iID,pEffect));
-
-		return true;
-	}
-
-	return false;
-}
- 
-void BEngine::AddEffectTech(UINT iEffectID, const char* pName, UINT iSubset)
-{
-	Effect& effect = m_Effects[iEffectID];
-	effect.m_hTech[iSubset] = effect.pEffect->GetTechniqueByName(pName);
-}
-
-void BEngine::AddEffectParameter(UINT iEffectID, const char* pName, const char* pKey)
-{
-	Effect& effect = m_Effects[iEffectID];
-	effect.m_hParameters[pKey] = effect.pEffect->GetParameterByName(0,pName);
-}
-
-// Load a mesh along with its mtrls and effects.
-bool BEngine::LoadXFile(UINT iXID, UINT iEffectID, const char* pFile)
-{
-	Mesh mesh;
-
-	ID3DXMesh* temp = 0;
-	ID3DXMesh* pMesh = 0;
-
-	ID3DXBuffer* pTempMtrl = 0;
-	ID3DXBuffer* pAdj = 0;
-
-	DWORD iNumMtrl = 0;
-
-	// Load Mesh from file
-	D3DXLoadMeshFromX(pFile,D3DXMESH_SYSTEMMEM,m_p3Device,&pAdj,&pTempMtrl,0,&iNumMtrl,&pMesh);
-
-	// change vertex format
-	CloneMesh(pMesh,&temp,D3DXMESH_SYSTEMMEM);
-	
-	pMesh = temp;
-	temp = 0;
-
-	// optimize mesh
-	pMesh->Optimize(D3DXMESH_MANAGED | D3DXMESHOPT_COMPACT |
-		D3DXMESHOPT_ATTRSORT | D3DXMESHOPT_VERTEXCACHE,
-		static_cast<DWORD*>(pAdj->GetBufferPointer()),0,0,0,&temp);
-
-	pMesh = temp;
-	temp = 0;
-
-	mesh.pMesh = pMesh;
-
-	// Get the size of the attribute buffer
-	DWORD iAttribSize = 0;
-	pMesh->GetAttributeTable(0,&iAttribSize);
-
-	// alloc enough memory for all of the mtrls
-	mesh.mrtl.resize(iAttribSize + 1);
-
-	// Get Effect
-	{
-		Effect& eff = m_Effects[iEffectID];
-		mesh.pEffect->pEffect = eff.pEffect;
-	}
-
-	// Get Pointer to mtrl to extract it
-	D3DXMATERIAL* pMtrl = static_cast<D3DXMATERIAL*>(pTempMtrl->GetBufferPointer());
-
-	for(DWORD i = 0; i < iAttribSize; ++i)
-	{
-		// Load Texture
-		IDirect3DTexture9* pTexture = 0;
-
-		// If There is a texture
-		if(pMtrl[i].pTextureFilename)
-		{
-			D3DXCreateTextureFromFile(m_p3Device,pMtrl[i].pTextureFilename,&pTexture);
-		}
-
-		mesh.mrtl[i].mtrl = pMtrl[i].MatD3D;
-		mesh.mrtl[i].pTexture = pTexture;
-	}
-
-	m_Meshes.insert(make_pair(iXID,mesh));
-	return true;
-}
-
-bool BEngine::RenderMesh(UINT iID) const
-{
-	auto& mesh = m_Meshes.find(iID)->second;
-	auto* pEffect = mesh.pEffect->pEffect;
-
-	auto& parameters = mesh.pEffect->m_hParameters;
-	auto& tech = mesh.pEffect->m_hTech;
-
-	// Create World Matrix
-	D3DXMATRIX W;
-	D3DXMatrixTranslation(&W,mesh.pos.x,mesh.pos.y,mesh.pos.z);
-
-	// World Inverse Transpose
-	D3DXMATRIX WIT;
-	D3DXMatrixInverse(&WIT,NULL,&W);
-	D3DXMatrixTranspose(&WIT,&WIT);
-
-	pEffect->SetMatrix(parameters["World"],&W);
-	pEffect->SetMatrix(parameters["WIT"],&WIT);
-	//pEffect->SetMatrix(parameters["WVP"],&(*pVP * W));
-
-	// Loop through all of the subsets
-	for(int j = 0; j < m_Meshes.size(); ++j)
-	{
-		pEffect->SetTechnique(tech[j]);
-
-		pEffect->SetValue(parameters["gObjectMrtl"],(void*)&mesh.mrtl[j].mtrl,sizeof(D3DMATERIAL9));
-		pEffect->SetTexture(parameters["gTex"],mesh.mrtl[j].pTexture);
-
-		UINT i = 0;
-		pEffect->Begin(&i,0);
-
-		for(int p = 0; p < i; ++p)
-		{
-			pEffect->BeginPass(p);
-
-			mesh.pMesh->DrawSubset(j);
-		
-			pEffect->EndPass();
-		}
-		pEffect->End();
 	}
 
 	return true;
-}*/
+}
+
+
 
 void BEngine::InitializeWindows(HINSTANCE hInstance, const string& winCaption)
 {
@@ -377,104 +162,6 @@ void BEngine::InitializeWindows(HINSTANCE hInstance, const string& winCaption)
 	UpdateWindow(m_hWindowHandle);
 }
 
-void BEngine::InitializeDirectX()
-{
-	// Step 1: Create the IDirect3D9 object.
-
-    m_pDirect3D = Direct3DCreate9(D3D_SDK_VERSION);
-	if( !m_pDirect3D ) { throw std::string("Direct3DCreate9 Failed"); }
-
-	EnumerateDisplayAdaptors();
-
-	// Fill out the D3DPRESENT_PARAMETERS structure.
-	
-	m_D3DParameters.BackBufferWidth            = 0;
-	m_D3DParameters.BackBufferHeight           = 0;
-	m_D3DParameters.BackBufferFormat           = D3DFMT_UNKNOWN;
-	m_D3DParameters.BackBufferCount            = 1;
-	m_D3DParameters.MultiSampleType            = D3DMULTISAMPLE_NONE;
-	m_D3DParameters.MultiSampleQuality         = 0;
-	m_D3DParameters.SwapEffect                 = D3DSWAPEFFECT_DISCARD; 
-	m_D3DParameters.hDeviceWindow              = m_hWindowHandle;
-	m_D3DParameters.Windowed                   = true;
-	m_D3DParameters.EnableAutoDepthStencil     = true;
-	m_D3DParameters.AutoDepthStencilFormat     = D3DFMT_D24S8;
-	m_D3DParameters.Flags                      = 0;
-	m_D3DParameters.FullScreen_RefreshRateInHz = 0;
-	m_D3DParameters.PresentationInterval       = D3DPRESENT_INTERVAL_IMMEDIATE;//D3DPRESENT_INTERVAL_IMMEDIATE, D3DPRESENT_INTERVAL_DEFAULT
-
-	HRESULT hPass = m_pDirect3D->CheckDeviceType(D3DADAPTER_DEFAULT,D3DDEVTYPE_HAL,D3DFMT_X8R8G8B8,D3DFMT_X8R8G8B8,false);
-
-	if(hPass == D3DERR_NOTAVAILABLE) { throw string("Device not supported."); }
-	
-	//Create the device.
-
-	m_pDirect3D->CreateDevice(
-		D3DADAPTER_DEFAULT, // primary adapter
-		D3DDEVTYPE_HAL,           // device type
-		m_hWindowHandle,          // window associated with device
-		D3DCREATE_HARDWARE_VERTEXPROCESSING,   // vertex processing
-	    &m_D3DParameters,            // present parameters
-	    &m_p3Device);      // return created device
-}
-
-/*void BEngine::CloneMesh(ID3DXMesh* pMesh, ID3DXMesh** ppClonedMesh , DWORD options)
-{
-	// New Vertex decl
-	D3DVERTEXELEMENT9 elements[MAXD3DDECLLENGTH];
-	UINT iElements = 0;
-	VertexNPos::m_pVertexDecl->GetDeclaration(elements,&iElements);
-
-	// new cloned interface
-	pMesh->CloneMesh(options,elements,m_p3Device,ppClonedMesh);
-
-	// Free memory
-	pMesh->Release();
-}*/
-
-bool BEngine::IsDeviceLost()
-{
-	// Get the state of the graphics device.
-	HRESULT hr = m_p3Device->TestCooperativeLevel();
-
-	// If the device is lost and cannot be reset yet then
-	// sleep for a bit and we'll try again on the next 
-	// message loop cycle.
-	if( hr == D3DERR_DEVICELOST )
-	{
-		Sleep(20);
-		return true;
-	}
-	// Driver error, exit.
-	else if( hr == D3DERR_DRIVERINTERNALERROR )
-	{
-		MessageBox(0, "Internal Driver Error...Exiting", 0, 0);
-		PostQuitMessage(0);
-		return true;
-	}
-	// The device is lost but we can reset and restore it.
-	else if( hr == D3DERR_DEVICENOTRESET )
-	{
-		OnLostDevice();
-		m_p3Device->Reset(&m_D3DParameters);
-		OnResetDevice();
-		return false;
-	}
-	else
-	{
-		return false;
-	}
-}
-
-void BEngine::OnLostDevice()
-{
-	 m_pFont->OnLostDevice();
-}
-
-void BEngine::OnResetDevice()
-{
-	m_pFont->OnResetDevice();
-}
 
 void BEngine::MsgProc(UINT msg, WPARAM wParam, LPARAM lparam)
 {
--- Game Engine/BEngine.h
+++ Game Engine/BEngine.h
@@ -1,51 +1,12 @@
 
 #pragma once
-
 #include "StdAfx.h"
 
-#include "BCamera.h"
 #include "Interfaces.h"
 
 using namespace std;
 using namespace stdext;
 
-/*enum EEffect
-{
-	// Transformations
-	World,
-	WorldViewProj,
-	WorldInverseTranspose,
-
-};
-
-
-
-struct Effect
-{
-
-	ID3DXEffect* pEffect;
-
-	D3DXHANDLE* pTech;
-	unsigned int iNumTech;
-
-	// maybe can use ints in the future for a faster search, with enums - they are not dynamic ???
-	hash_map<UINT,D3DXHANDLE> m_hTech;
-	hash_map<string,D3DXHANDLE> m_hParameters;
-};
-
-struct Mesh
-{
-	ID3DXMesh* pMesh;
-	Effect* pEffect;
-	IFunct* pFunct;
-	
-	D3DXVECTOR3 pos;
-	
-	vector<Mtrl> mrtl;
-};*/
-
-
-// Base pugin
 
 
 // ************************
@@ -53,60 +14,33 @@ struct Mesh
 // All of the different sub parts of the engine should be independent, they should be their own dll.
 // Then this class would load the dll's dynamically and be the leader of the game without knowing its implementation details.
 // ***********************
-class BEngine : public RefCounting
+class BEngine
 {
 public:
 
+	static BEngine* GetInstance();
+
 	// Initializes DirextX along with Windows
-	BEngine(HINSTANCE hInstance,const string& winCaption);
-	~BEngine();
+	static void Initialize(HINSTANCE hInstance,const string& winCaption);
+	
+	// Deletes everything from memory
+	static void Delete();
 	
 	bool Update();
 	void Present();
 
-	void EnumerateDisplayAdaptors();
-
-	// effects              
-	/*bool LoadEffect(UINT iID,const char* pFile);
-	
-	void AddEffectTech(UINT iEffectID, const char* pName, UINT iSubset); // ?? questionable
-	void AddEffectParameter(UINT iEffectID, const char* pName, const char* pKey);
-	//bool AddEffectParameter(UINT iEffectID, const char* pName, UINT iIndex, const char* pKey);
-
-	// Mesh
-	bool LoadXFile(UINT iXID, UINT iEffectID, const char* pFile);
-	bool RenderMesh(UINT iID) const;
-	
-	// Texture
-	bool LoadTexture(UINT iID, const char* pFile);
-	bool AutoGenSphereTexCoords(UINT iMesh);
-	bool AutoGenCyclTexCoords(UINT iMesh, int axis);*/
-
-	void DrawText(char* p);
-
-	IDirect3DDevice9* GetDevice();
-
 	void MsgProc(UINT msg, WPARAM wPraram, LPARAM lparam);
 
-	HINSTANCE GetHINSTANCE() const { return m_hInstance; }
-	HWND GetWindowHandle() const { return m_hWindowHandle; }
+	HINSTANCE GetHINSTANCE() const;
+	HWND GetWindowHandle() const;
 
 private:
 
 	// Single Instance of the class.
 	static BEngine* s_pInstance;
 
-	IDirect3DDevice9* m_p3Device;
-	IDirect3D9* m_pDirect3D;
-	ID3DXFont* m_pFont;
-
-	vector<D3DDISPLAYMODE> m_mode;
-
-	D3DPRESENT_PARAMETERS m_D3DParameters;
 	HWND m_hWindowHandle;
 	HINSTANCE m_hInstance;
-
-	DWORD m_ClearBuffers;
 
 	bool m_bPaused;
 
@@ -114,91 +48,25 @@ private:
 	float m_fStartCount;
 	float m_fSecsPerCount;
 
-	/*hash_map<UINT,Mesh> m_Meshes;
-	hash_map<UINT,Effect> m_Effects;
-	hash_map<UINT,IDirect3DTexture9*> m_Textures;*/
-
-	/*
-	Enter something here!!!///////
-	*/
+	BEngine(HINSTANCE hInstance,const string& winCaption);
+	~BEngine();
 
 	// Prevent copies from being made.
 	BEngine(const BEngine&);
 	BEngine operator =(const BEngine&);
 
 	void InitializeWindows(HINSTANCE hInstance, const string& winCaption);
-	void InitializeDirectX();
+	
 
 	bool Begin();
 	bool End();
 
-	void RenderOptions();
-
-	//void InitalizeVertexFormat(); //???
-
-	//void CloneMesh(ID3DXMesh* pMesh, ID3DXMesh** pClonedMesh, DWORD options);
 	
 	bool IsDeviceLost();
 	void OnLostDevice();
 	void OnResetDevice();
 
-	//void EnableFullscreen(bool b);
+	
 
 };
 
-extern BEngine* g_pEngine;
-
-/*
-
-// Switch to fullscreen mode.
-	if( enable )
-	{
-		// Are we already in fullscreen mode?
-		if( !m_D3DParameters.Windowed ) 
-			return;
-
-		int width  = GetSystemMetrics(SM_CXSCREEN);
-		int height = GetSystemMetrics(SM_CYSCREEN);
-
-		m_D3DParameters.BackBufferFormat = D3DFMT_X8R8G8B8;
-		m_D3DParameters.BackBufferWidth  = width;
-		m_D3DParameters.BackBufferHeight = height;
-		m_D3DParameters.FullScreen_RefreshRateInHz = 60;
-		m_D3DParameters.Windowed         = false;
-
-		// Change the window style to a more fullscreen friendly style.
-		SetWindowLongPtr(m_hWindowHandle, GWL_STYLE, WS_POPUP);
-
-		// If we call SetWindowLongPtr, MSDN states that we need to call
-		// SetWindowPos for the change to take effect.  In addition, we 
-		// need to call this function anyway to update the window dimensions.
-		SetWindowPos(m_hWindowHandle, HWND_TOP, 0, 0, width, height, SWP_NOZORDER | SWP_SHOWWINDOW);	
-	}
-	// Switch to windowed mode.
-	else
-	{
-		// Are we already in windowed mode?
-		if( m_D3DParameters.Windowed ) 
-			return;
-
-		RECT R = {0, 0, 800, 600};
-		AdjustWindowRect(&R, WS_OVERLAPPEDWINDOW, false);
-		m_D3DParameters.BackBufferFormat = D3DFMT_UNKNOWN;
-		m_D3DParameters.BackBufferWidth  = 800;
-		m_D3DParameters.BackBufferHeight = 600;
-		m_D3DParameters.FullScreen_RefreshRateInHz = 0;
-		m_D3DParameters.Windowed         = true;
-	
-		// Change the window style to a more windowed friendly style.
-		SetWindowLongPtr(m_hWindowHandle, GWL_STYLE, WS_OVERLAPPEDWINDOW);
-
-		// If we call SetWindowLongPtr, MSDN states that we need to call
-		// SetWindowPos for the change to take effect.  In addition, we 
-		// need to call this function anyway to update the window dimensions.
-		SetWindowPos(m_hWindowHandle, HWND_TOP, 100, 100, R.right, R.bottom, SWP_NOZORDER | SWP_SHOWWINDOW);
-	}
-
-	// Reset the device with the changes.
-	OnLostDevice();
-	g_p3dDevice->Reset(&m_D3DParameters);
-	OnResetDevice();*/
