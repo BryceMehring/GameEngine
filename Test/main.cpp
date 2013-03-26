f57cd3eb5d59de77ed370f412079b001b034d966
Bryce Mehring <Bryce Mehring@d47a555f-5172-324c-b6bc-1cff69a52d8b>
2013-03-26 22:30:48 +0000

    Removing Old Code

--- Test/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "SingleInstance.h"
-
-#pragma comment(lib,"Game Engine.lib")
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
-{
-	SingleInstance instance;
-	return instance.Run(hInstance,lpCmdLine);
-}
