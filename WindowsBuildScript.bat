cd1e485c22dd78833c239074c869a7ea6db788fb
f12dude <bm4g5@mst.edu>
2013-07-20 17:09:16 -0500

    Updated Windows Build Script
    The new Windows build script automatically creates the symbolic links
    Updated the gitignore file to ignore the bin dir

--- WindowsBuildScript.bat
+++ WindowsBuildScript.bat
@@ -1,2 +1,18 @@
+cd %~dp0
 cmake -G "Visual Studio 11" CMakeLists.txt
+
+mkdir bin && cd bin && mkdir Debug && cd Debug
+
+mklink /D textures ..\..\textures
+mklink /D shaders ..\..\shaders
+mklink glfw3.dll ..\..\..\glfw\lib-msvc110\glfw3.dll
+mklink glew32.dll ..\..\..\glew\bin\glew32.dll
+
+cd ..\ && mkdir Release && cd Release
+
+mklink /D textures ..\..\textures
+mklink /D shaders ..\..\shaders
+mklink glfw3.dll ..\..\..\glfw\lib-msvc110\glfw3.dll
+mklink glew32.dll ..\..\..\glew\bin\glew32.dll
+
 pause
