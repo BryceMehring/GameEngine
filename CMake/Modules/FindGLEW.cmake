772f18271534ea4cbefd3ddd66d1bce789287825
f12dude <bm4g5@mst.edu>
2013-08-07 00:23:35 -0500

    Fixed GLEW finding on linux

--- CMake/Modules/FindGLEW.cmake
+++ CMake/Modules/FindGLEW.cmake
@@ -1,10 +1,12 @@
 
 #find GLEW
 
-find_path(GLEW_INCLUDE_DIR glew.h "${CMAKE_SOURCE_DIR}/../glew/include/GL/")
-find_library(GLEW_LIBRARY glew32 "${CMAKE_SOURCE_DIR}/../glew/lib/Release/Win32/")
+    find_path(GLEW_INCLUDE_DIR GL/glew.h "${CMAKE_SOURCE_DIR}/../glew/include/GL/glew.h")
+    find_library(GLEW_LIBRARY NAMES GLEW glew32 PATHS "${CMAKE_SOURCE_DIR}/../glew/lib/Release/Win32/" PATH_SUFFIXES lib64)
 
-set(GLEW_INCLUDE_DIR ${GLEW_INCLUDE_DIR}/../)
+if(WIN32)
+    set(GLEW_INCLUDE_DIR ${GLEW_INCLUDE_DIR}/../)
+endif(WIN32)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GLEW DEFAULT_MSG GLEW_INCLUDE_DIR GLEW_LIBRARY)
