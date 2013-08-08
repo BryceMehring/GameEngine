02043d1658168b8e3317685a1ebcec3a1e1e6933
f12dude <bm4g5@mst.edu>
2013-08-07 21:44:17 -0500

    Really fixed finding glew on both windows and linux

--- CMake/Modules/FindGLEW.cmake
+++ CMake/Modules/FindGLEW.cmake
@@ -1,12 +1,12 @@
 
 #find GLEW
 
-find_path(GLEW_INCLUDE_DIR glew.h "${CMAKE_SOURCE_DIR}/../glew/include/GL/" GL/)
+find_path(GLEW_INCLUDE_DIR GL/glew.h
+	/usr/include 
+	"${CMAKE_SOURCE_DIR}/../glew/include/"
+	)
+	
 find_library(GLEW_LIBRARY NAMES GLEW glew32 PATHS "${CMAKE_SOURCE_DIR}/../glew/lib/Release/Win32/" PATH_SUFFIXES lib64)
-
-if(WIN32)
-    set(GLEW_INCLUDE_DIR ${GLEW_INCLUDE_DIR}/../)
-endif(WIN32)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GLEW DEFAULT_MSG GLEW_INCLUDE_DIR GLEW_LIBRARY)
