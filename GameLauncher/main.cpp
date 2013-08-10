fc8ee7fbfd04b0d04b1d7c13ba5489fc518488ba
f12dude <bm4g5@mst.edu>
2013-08-10 02:18:03 -0500

    Fixed building on linux

--- GameLauncher/main.cpp
+++ GameLauncher/main.cpp
@@ -1,4 +1,4 @@
-//#include "Game.h"
+#include "Game.h"
 
 #include <iostream>
 #include <ctime>
@@ -7,7 +7,6 @@ using namespace std;
 
 int main(int size, char** cmd)
 {
-	system("pause");
 	if(size < 2)
 	{
 		cout<<"Invalid Number of command line arguments"<<endl;
@@ -20,10 +19,8 @@ int main(int size, char** cmd)
 
 	// seed the random number generator
 	srand (time(0));
-	
-	return 0;
 
-	//Game myGame;
-	//myGame.SetNextState(pState);
-	//return myGame.Run();
+	Game myGame;
+	myGame.SetNextState(pState);
+	return myGame.Run();
 }
