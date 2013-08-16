29d75ba817e4ae6d1236d4609a542de96986b661
Bryce Mehring <bm4g5@mst.edu>
2013-08-15 23:47:19 -0500

    Fixed bug in the windows bat file

--- CreateWinSymLinks.bat
+++ CreateWinSymLinks.bat
@@ -1,10 +1,14 @@
 @ECHO OFF
 cd %~dp0
 
+cd GameLauncher
+
+mklink base.r ..\base.r 
+
+cd ..
+
 mkdir bin && cd bin && mkdir Debug && cd Debug
 
-mklink /D textures ..\..\textures
-mklink /D shaders ..\..\shaders
 mklink glfw3.dll ..\..\..\glfw\lib-msvc110\glfw3.dll
 mklink glew32.dll ..\..\..\glew\bin\Release\Win32\glew32.dll
 
