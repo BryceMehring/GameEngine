003f4ec506cb538b3ba101a204e346855b2c7d62
Bryce Mehring <bm4g5@mst.edu>
2013-08-20 17:17:09 -0500

    Fixed CMake error with angelscript on windows

--- CMake/Modules/FindANGELSCRIPT.cmake
+++ CMake/Modules/FindANGELSCRIPT.cmake
@@ -3,7 +3,9 @@
 find_path(ANGELSCRIPT_INCLUDE_DIR angelscript.h "${CMAKE_SOURCE_DIR}/../angelscript/angelscript/include/")
 find_path(ANGELSCRIPT_ADDON_INCLUDE_DIR scriptmath/ "${CMAKE_SOURCE_DIR}/../angelscript/add_on")
 find_library(ANGELSCRIPT_LIBRARY_RELEASE angelscript "${CMAKE_SOURCE_DIR}/../angelscript/angelscript/lib/")
-#find_library(ANGELSCRIPT_LIBRARY_DEBUG angelscriptd "${CMAKE_SOURCE_DIR}/../angelscript/angelscript/lib/")
+if(MSVC)
+	find_library(ANGELSCRIPT_LIBRARY_DEBUG angelscriptd "${CMAKE_SOURCE_DIR}/../angelscript/angelscript/lib/")
+endif(MSVC)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ANGELSCRIPT REQUIRED_VARS ANGELSCRIPT_INCLUDE_DIR ANGELSCRIPT_ADDON_INCLUDE_DIR ANGELSCRIPT_LIBRARY_RELEASE)
