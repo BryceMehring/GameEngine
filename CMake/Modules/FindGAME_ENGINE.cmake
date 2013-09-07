86b53bb272ccde3101a90702c3b29163838e0cdc
Bryce Mehring <bm4g5@mst.edu>
2013-09-07 15:09:36 -0500

    Fixed finding the game engine in cmake

--- CMake/Modules/FindGAME_ENGINE.cmake
+++ CMake/Modules/FindGAME_ENGINE.cmake
@@ -5,13 +5,13 @@ find_path(GAME_ENGINE_INCLUDE_DIR IGameState.h
 	"${CMAKE_SOURCE_DIR}/../GameEngine/GameEngine"
 	)
 	
-find_path(GAME_LAUNCHER_INCLUDE_DIR interfaces/IGame.h
-	"${CMAKE_SOURCE_DIR}/../GameEngine/GameLauncher"
+find_path(GAME_COMMON_INCLUDE_DIR interfaces/IGame.h
+	"${CMAKE_SOURCE_DIR}/../GameEngine/common"
 	)
 	
 find_library(GAME_ENGINE_LIBRARY NAMES GameEngine PATHS "${CMAKE_SOURCE_DIR}/../GameEngine/bin")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GAME_ENGINE REQUIRED_VARS GAME_ENGINE_INCLUDE_DIR GAME_ENGINE_LIBRARY)
+find_package_handle_standard_args(GAME_ENGINE REQUIRED_VARS GAME_ENGINE_INCLUDE_DIR GAME_COMMON_INCLUDE_DIR GAME_ENGINE_LIBRARY)
 
-mark_as_advanced(GAME_ENGINE_INCLUDE_DIR GAME_ENGINE_LIBRARY)
+mark_as_advanced(GAME_ENGINE_INCLUDE_DIR GAME_ENGINE_LIBRARY GAME_COMMON_INCLUDE_DIR)
