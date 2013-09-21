9a0f54ffc4c53a9cf20a39b0970e3248aa08e891
Bryce Mehring <bm4g5@mst.edu>
2013-09-21 16:25:17 -0500

    Updated the base resource file with the new names for the sprite shaders

--- base.r
+++ base.r
@@ -1,6 +1,6 @@
 shader lineShader shaders/LineVertexShader.vert shaders/LinePixelShader.frag
 shader textShader shaders/TextVertexShader.vert shaders/TextPixelShader.frag
-shader sprite shaders/vertexShader.vert shaders/PixelShader.frag
+shader sprite shaders/SpriteVertexShader.vert shaders/SpritePixelShader.frag
 texture font textures/font.png
 
 
