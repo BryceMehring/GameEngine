6c68a139544ea7c89fa36a800c8db4867e88ced7
Bryce Mehring <bm4g5@mst.edu>
2013-09-28 15:36:26 -0500

    Updated the base resource file to support the new resource file format Enabled rendering of fonts again

--- base.r
+++ base.r
@@ -1,7 +1,7 @@
 shader lineShader shaders/LineVertexShader.vert shaders/LinePixelShader.frag
 shader textShader shaders/TextVertexShader.vert shaders/TextPixelShader.frag
 shader sprite shaders/SpriteVertexShader.vert shaders/SpritePixelShader.frag
-texture font textures/font.png
+font font textures/font.png
 
 
 
