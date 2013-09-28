a1fb9fa29d44275c6dfe383a6992e9f00472f1c5
Bryce Mehring <bm4g5@mst.edu>
2013-09-27 23:45:00 -0500

    Added GLFW error handling

--- GameEngine/GLFWInit.cpp
+++ GameEngine/GLFWInit.cpp
@@ -1,9 +1,18 @@
 #include "GLFWInit.h"
 #include <GLFW/glfw3.h>
 #include <cassert>
+#include "FileManager.h"
+
+static void ErrorCallback(int, const char* error)
+{
+	FileManager::Instance().WriteToLog(error);
+	abort();
+}
 
 GLFWInit::GLFWInit()
 {
+	glfwSetErrorCallback(ErrorCallback);
+
 	assert(glfwInit() == GL_TRUE);
 }
 
