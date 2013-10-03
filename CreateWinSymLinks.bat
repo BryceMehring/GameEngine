f7a2293c3b51b10ba3befe4c44d44906b702a331
Bryce Mehring <bm4g5@mst.edu>
2013-10-02 20:10:48 -0500

    Added missing cmake file Fixed windows bat file

--- CreateWinSymLinks.bat
+++ CreateWinSymLinks.bat
@@ -3,10 +3,10 @@ cd %~dp0
 
 mkdir bin && cd bin
 
-mklink /D textures ..\..\textures
-mklink /D shaders ..\..\shaders
+mklink /D textures ..\textures
+mklink /D shaders ..\shaders
 
-mklink glfw3.dll ..\..\..\glfw\lib-msvc110\glfw3.dll
-mklink glew32.dll ..\..\..\glew\bin\Release\Win32\glew32.dll
+mklink glfw3.dll ..\..\glfw\lib-msvc110\glfw3.dll
+mklink glew32.dll ..\..\glew\bin\Release\Win32\glew32.dll
 
 pause
