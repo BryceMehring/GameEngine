779122796c1c328ff4ec81672f4ab6d48453bf53
Bryce Mehring <bm4g5@mst.edu>
2014-01-23 03:38:00 -0600

    Removed another old example

--- Examples/Pong/resource.r
+++
@@ -1,8 +0,0 @@
-texture ..\\textures\\flame_sprite.png
-texture ..\\textures\\paddle.png
-texture ..\\textures\\textbox3.jpg
-texture ..\\textures\\ball.png
-texture ..\\textures\\font.png
-texture ..\\textures\\font2.png
-texture ..\\textures\\font3.png
-shader ..\\shaders\\2DShader.fx
