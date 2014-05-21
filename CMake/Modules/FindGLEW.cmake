9cb25623acfb708a4a184b9b993987318de284d3
Bryce Mehring <bm4g5@mst.edu>
2014-05-20 21:57:19 -0500

    Renamed variable in CMake build script
    GLEW_LIBRARY changed to GLEW_STATIC_LIBRARY to reduce ambiguity

--- CMake/Modules/FindGLEW.cmake
+++ CMake/Modules/FindGLEW.cmake
@@ -6,9 +6,9 @@ find_path(GLEW_INCLUDE_DIR GL/glew.h
 	"${CMAKE_SOURCE_DIR}/../glew/include"
 	)
 	
-find_library(GLEW_LIBRARY NAMES GLEW glew32)
+find_library(GLEW_STATIC_LIBRARY NAMES GLEW glew32)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GLEW REQUIRED_VARS GLEW_INCLUDE_DIR GLEW_LIBRARY)
+find_package_handle_standard_args(GLEW REQUIRED_VARS GLEW_INCLUDE_DIR GLEW_STATIC_LIBRARY)
 
-mark_as_advanced(GLEW_INCLUDE_DIR GLEW_LIBRARY)
+mark_as_advanced(GLEW_INCLUDE_DIR GLEW_STATIC_LIBRARY)
