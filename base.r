30790e83742548b42aa5d083e9ca5c658edba5f4
Bryce Mehring <bm4g5@mst.edu>
2014-06-01 23:33:09 -0500

    Added button to base.r

--- base.r
+++ base.r
@@ -2,6 +2,7 @@ shader lineShader shaders/LineVertexShader.vert shaders/LinePixelShader.frag
 shader textShader shaders/TextVertexShader.vert shaders/TextPixelShader.frag
 shader sprite shaders/SpriteVertexShader.vert shaders/SpritePixelShader.frag
 font font textures/font.png
+texture button textures/button.png
 
 
 
