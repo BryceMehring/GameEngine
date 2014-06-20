5cf0e98b3ab1f4482277b6733a128fd62ecc9442
Bryce Mehring <bm4g5@mst.edu>
2014-06-20 05:28:09 -0500

    Attempt at fixing cross compiling header errors

--- source/GameEngine/PluginManager.cpp
+++ source/GameEngine/PluginManager.cpp
@@ -10,7 +10,7 @@
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#include <windows.h>
 
 //Returns the last Win32 error, in string format. Returns an empty string if there is no error.
 std::string GetLastErrorAsString()
