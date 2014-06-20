5eba40e8baadb9b98382a56bdcadb2d5ff0c742c
Bryce Mehring <bm4g5@mst.edu>
2014-06-20 18:54:33 -0500

    Added the ability to pack examples with cpack Added the ability to pack Mingw dependencies

---
+++ CMake/Modules/FindMINGW_DEPENDS.cmake
@@ -0,0 +1,9 @@
+
+#find MINGW_DEPENDS_DIR
+
+find_path(MINGW_DEPENDS_DIR NAMES libstdc++-6.dll libgcc_s_sjlj-1.dll libwinpthread-1.dll)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(MINGW_DEPENDS REQUIRED_VARS MINGW_DEPENDS_DIR)
+
+mark_as_advanced(MINGW_DEPENDS_DIR)
