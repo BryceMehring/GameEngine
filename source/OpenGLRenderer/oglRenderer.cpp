3bf10c2b32e7ce3d88062a32e407ac29eeacbe3c
Bryce Mehring <bm4g5@mst.edu>
2014-06-28 17:32:26 -0500

    Removed forward compat context on windows

--- source/OpenGLRenderer/oglRenderer.cpp
+++ source/OpenGLRenderer/oglRenderer.cpp
@@ -364,10 +364,6 @@ void oglRenderer::GLFWOpenWindowHints()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-#ifdef _WIN32
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
