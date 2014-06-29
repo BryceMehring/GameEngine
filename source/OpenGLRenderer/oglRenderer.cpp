d00da85b43f1fa491d100b3d6999845f9a9b60fc
Bryce Mehring <bm4g5@mst.edu>
2014-06-29 16:55:11 -0500

    Removed default hints for opengl context

--- source/OpenGLRenderer/oglRenderer.cpp
+++ source/OpenGLRenderer/oglRenderer.cpp
@@ -361,9 +361,6 @@ void oglRenderer::MonitorCallback(GLFWmonitor* monitor, int state)
 
 void oglRenderer::GLFWOpenWindowHints()
 {
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
